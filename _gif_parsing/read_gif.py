94186b1b2aa22fd6f2bdc1a77cf733ba7ae3770c
Mark <wernsdorfer@gmail.com>
2018-09-26 08:48:03 +0200

    finished gif example generator

--- _gif_parsing/read_gif.py
+++ _gif_parsing/read_gif.py
@@ -1,17 +1,19 @@
 # coding=utf-8
 import os
+import time
+from typing import Generator, Tuple
 
 from PIL import Image
 
 from tools.load_configs import Config
 
 
-def generate_rbg_pixels(file_path, window_size=1):
+def generate_rbg_pixels(file_path, window_size=1) -> Generator[Tuple[Tuple[int, int, int], ...], None, None]:
     assert file_path.endswith(".gif")
     frame = Image.open(file_path)
     n_frames = 0
 
-    square = window_size ** 2.
+    square = window_size ** 2
     while frame:
         frame_rgb = frame.convert("RGB")
 
@@ -20,7 +22,7 @@ def generate_rbg_pixels(file_path, window_size=1):
             for _x in range(0, (frame_rgb.width // window_size) * window_size, window_size):
                 window = tuple(frame_rgb.getpixel((_x + __x, _y + __y)) for __y in range(window_size) for __x in range(window_size))
                 _r, _g, _b = zip(*window)
-                each_pixel = round(sum(_r) / square), round(sum(_g) / square), round(sum(_b) / square)
+                each_pixel = sum(_r) // square, sum(_g) // square, sum(_b) // square
                 pixels.append(each_pixel)
         yield tuple(pixels)
 
@@ -31,16 +33,16 @@ def generate_rbg_pixels(file_path, window_size=1):
             n_frames = 0
 
 
-def generate_grayscale_pixels(file_path, window_size=1):
-    for each_frame in generate_rbg_pixels(file_path, window_size=window_size):
-        yield tuple(round(sum(_p) / 3) for _p in each_frame)
+def generate_grayscale_pixels(pixel_generator) -> Generator[Tuple[int, ...], None, None]:
+    for each_frame in pixel_generator:
+        yield tuple(sum(_p) // 3 for _p in each_frame)
 
 
 def generate_pixel_examples(pixel_generator):
     last_pixels = next(pixel_generator)
     while True:
         pixels = next(pixel_generator)
-        yield zip(last_pixels, pixels)
+        yield tuple(zip(last_pixels, pixels))
         last_pixels = pixels
 
 
@@ -64,10 +66,24 @@ def write_image(greyscale_pixels, width, height, file_path):
     frame.save(file_path, os.path.splitext(file_path)[-1][1:])
 
 
-if __name__ == "__main__":
+def check_frames():
     config = Config("../configs/config.json")
     size = 5
     original = Image.open(config["data_dir"] + "gifs/tenor.gif")
-    for no_frame, values in enumerate(generate_grayscale_pixels(config["data_dir"] + "gifs/tenor.gif", window_size=size)):
+    for no_frame, values in enumerate(generate_grayscale_pixels(generate_rbg_pixels(config["data_dir"] + "gifs/tenor.gif", window_size=size))):
         print(f"processing frame {no_frame}...")
         write_image(values, original.width // size, original.height // size, "{:03d}.png".format(no_frame))
+
+
+def check_examples():
+    config = Config("../configs/config.json")
+    size = 5
+    # pixel_generator = generate_grayscale_pixels(generate_rbg_pixels(config["data_dir"] + "gifs/tenor.gif", window_size=size))
+    pixel_generator = generate_rbg_pixels(config["data_dir"] + "gifs/tenor.gif", window_size=size)
+    for concurrent_examples in generate_pixel_examples(pixel_generator):
+        print(concurrent_examples[0])
+        time.sleep(.5)
+
+
+if __name__ == "__main__":
+    check_examples()
