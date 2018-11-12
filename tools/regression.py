7d28b96d43b16f645360b4bf46139fbda2f9dcac
Mark <wernsdorfer@gmail.com>
2018-11-12 14:59:32 +0100

    started hill climbing regression

--- tools/regression.py
+++ tools/regression.py
@@ -2,7 +2,7 @@
 import itertools
 import random
 from functools import reduce
-from typing import Sequence, Tuple, Callable
+from typing import Sequence, Tuple, Callable, List
 
 from matplotlib import pyplot
 
@@ -10,13 +10,10 @@ from tools.functionality import combinations, smear
 from tools.timer import Timer
 
 
-class Regression:
+class MultipleRegression:
     def __init__(self, input_dimensionality: int, drag: int):
         self._in_dim = input_dimensionality
         self._drag = drag
-
-    def fitted_function(self) -> Callable[[Sequence[float]], float]:
-        raise NotImplementedError()
 
     def _fit(self, input_values: Sequence[float], output_value: float, drag: int):
         raise NotImplementedError()
@@ -27,15 +24,16 @@ class Regression:
         _drag = max(drag, self._drag)
         self._fit(input_values, output_value, _drag)
 
+    def _output(self, input_values: Sequence[float]) -> float:
+        raise NotImplementedError()
+
     def output(self, input_values: Sequence[float]) -> float:
         assert len(input_values) == self._in_dim
-        fit = self.fitted_function()
-        return fit(input_values)
+        return self._output(input_values)
 
 
-class LinearRegression(Regression):
+class SingleLinearRegression:
     def __init__(self, drag: int):
-        super().__init__(1, drag)
         self._drag = drag
         self._mean_x = 0.
         self._mean_y = 0.
@@ -44,50 +42,34 @@ class LinearRegression(Regression):
         self._cov_xy = 0.
         self._initial = True
 
-    def _fit(self, input_values: Sequence[float], output_value: float, drag: int = -1):
+    def fit(self, input_value: float, output_value: float, drag: int = -1):
         assert self._drag >= 0 or drag >= 0
-        assert len(input_values) == 1
-        x = input_values[0]
         _drag = max(self._drag, drag)
 
         dy = output_value - self._mean_y
-        dx = x - self._mean_x
+        dx = input_value - self._mean_x
         self._var_x = smear(self._var_x, dx ** 2., _drag)
         self._cov_xy = smear(self._cov_xy, dx * dy, _drag)
         self._var_y = smear(self._var_y, dy ** 2., _drag)
 
         if self._initial:
-            self._mean_x = x
+            self._mean_x = input_value
             self._mean_y = output_value
             self._initial = False
 
-        self._mean_x = smear(self._mean_x, x, _drag)
+        self._mean_x = smear(self._mean_x, input_value, _drag)
         self._mean_y = smear(self._mean_y, output_value, _drag)
 
-    def fitted_function(self) -> Callable[[Sequence[float]], float]:
+    def output(self, input_values: float) -> float:
         a1 = 0. if self._var_x == 0. else self._cov_xy / self._var_x
         a0 = self._mean_y - a1 * self._mean_x
-        return lambda _x: a0 + a1 * _x[0]
+        return a0 + a1 * input_values
 
 
-class MultipleLinearRegression(Regression):
+class MultipleLinearRegression(MultipleRegression):
     def __init__(self, input_dimensionality: int, drag: int):
         super().__init__(input_dimensionality, drag)
-        self._regressions = tuple(LinearRegression(drag) for _ in range(input_dimensionality))
-
-    def fitted_function(self) -> Callable[[Sequence[float]], float]:
-        return lambda _x: sum(_regression.output([__x]) for _regression, __x in zip(self._regressions, _x)) / self._in_dim
-
-    def _fit(self, input_values: Sequence[float], output_value: float, drag: int):
-        for _each_input, _each_regression in zip(input_values, self._regressions):
-            _each_regression.fit([_each_input], output_value, drag=drag)
-
-
-class PolynomialRegression(MultipleLinearRegression):
-    def __init__(self, input_dimensionality: int, degree: int, drag: int):
-        no_polynomial_parameters = sum(combinations(_i + 1, _i + input_dimensionality) for _i in range(degree))
-        super().__init__(no_polynomial_parameters, drag)
-        self._degree = degree
+        self._regressions = tuple(SingleLinearRegression(drag) for _ in range(input_dimensionality))
 
     @staticmethod
     def _make_polynomial_inputs(input_values: Sequence[float], degree: int) -> Tuple[float, ...]:
@@ -107,25 +89,59 @@ class PolynomialRegression(MultipleLinearRegression):
             for each_combination in itertools.combinations_with_replacement(input_values, _i + 1)
         )
 
-    def _prediction(self, input_values: Sequence[float]) -> float:
-        _poly_inputs = PolynomialRegression._make_polynomial_inputs(input_values, self._degree)
-        poly_function = MultipleLinearRegression.fitted_function(self)
-        return poly_function(_poly_inputs)
-
-    def fitted_function(self) -> Callable[[Sequence[float]], float]:
-        return self._prediction
+    def _output(self, input_values: Sequence[float]) -> float:
+        return sum(_regression.output(_x) for _regression, _x in zip(self._regressions, input_values)) / self._in_dim
 
     def _fit(self, input_values: Sequence[float], output_value: float, drag: int):
-        _polynomial_inputs = PolynomialRegression._make_polynomial_inputs(input_values, self._degree)
-        MultipleLinearRegression._fit(self, _polynomial_inputs, output_value, drag)
+        for _each_input, _each_regression in zip(input_values, self._regressions):
+            _each_regression.fit(_each_input, output_value, drag=drag)
+
+
+class MultiplePolynomialFromLinearRegression(MultipleLinearRegression):
+    def __init__(self, input_dimensionality: int, degree: int, drag: int):
+        no_polynomial_parameters = sum(combinations(_i + 1, _i + input_dimensionality) for _i in range(degree))
+        super().__init__(no_polynomial_parameters, drag)
+        self._degree = degree
 
     def output(self, input_values: Sequence[float]):
-        poly_inputs = PolynomialRegression._make_polynomial_inputs(input_values, self._degree)
-        return Regression.output(self, poly_inputs)
+        poly_inputs = MultipleLinearRegression._make_polynomial_inputs(input_values, self._degree)
+        return MultipleRegression.output(self, poly_inputs)
 
     def fit(self, input_values: Sequence[float], output_value: float, drag: int = -1):
-        poly_inputs = PolynomialRegression._make_polynomial_inputs(input_values, self._degree)
-        Regression.fit(self, poly_inputs, output_value, drag=drag)
+        poly_inputs = MultipleLinearRegression._make_polynomial_inputs(input_values, self._degree)
+        MultipleRegression.fit(self, poly_inputs, output_value, drag=drag)
+
+
+class MultiplePolynomialHillClimbingRegression(MultipleRegression):
+    def __init__(self, input_dimensionality: int, degree: int, drag: int, epsilon: float):
+        super().__init__(input_dimensionality, drag)
+        self._no_polynomial_parameters = sum(combinations(_i + 1, _i + input_dimensionality) for _i in range(degree))
+        self._last_step = [0. for _ in range(self._no_polynomial_parameters)]
+        self._parameters = [0. for _ in range(self._no_polynomial_parameters)]
+        self._epsilon = epsilon
+        self._degree = degree
+
+    @staticmethod
+    def _randomize(vector: List[float], mean: float, deviance: float):
+        for _i in range(len(vector)):
+            vector[_i] += random.gauss(mean, deviance)
+
+    def _fit(self, input_values: Sequence[float], output_value: float, drag: int):
+        _prediction = self._output(input_values)
+        _error = _prediction - output_value
+        for _i, _s in enumerate(self._last_step):
+            self._last_step[_i] = smear(_s, _s - _error, drag)
+            # self._last_step[_i] -= _error
+
+        MultiplePolynomialHillClimbingRegression._randomize(self._last_step, 0., self._epsilon)
+
+        for _i, (_p, _s) in enumerate(zip(self._parameters, self._last_step)):
+            # self._parameters[_i] = smear(_p, _p + _s, drag)
+            self._parameters[_i] += _s
+
+    def _output(self, input_values: Sequence[float]) -> float:
+        poly_inputs = MultipleLinearRegression._make_polynomial_inputs(input_values, self._degree)
+        return sum(_p * _x for _p, _x in zip(poly_inputs, self._parameters))
 
 
 def setup_2d_axes():
@@ -145,7 +161,8 @@ def test_2d():
 
     plot_axis, error_axis = setup_2d_axes()
 
-    r = PolynomialRegression(1, 4, -1)
+    # r = MultiplePolynomialFromLinearRegression(1, 4, -1)
+    r = MultiplePolynomialHillClimbingRegression(1, 4, -1, 4.)
 
     fun = lambda _x: -11. + 4. * _x ** 1. + -7.2 * _x ** 2. + .4 * _x ** 3.
     x_range = tuple(_x / 10. for _x in range(int(dim_range[0]) * 10, int(dim_range[1]) * 10))
@@ -166,11 +183,10 @@ def test_2d():
         error = 0 if iterations < 1 else smear(error_development[-1], abs(y_o - y_t), iterations)
         error_development.append(error)
 
-        if Timer.time_passed(100):
+        if Timer.time_passed(1000):
             print(f"{iterations * 100. / total_time:05.2f}% finished")
 
-            fitted_function = r.fitted_function()
-            l, = plot_axis.plot(x_range, tuple(fitted_function([_x]) for _x in x_range), color="C1")
+            l, = plot_axis.plot(x_range, tuple(r.output([_x]) for _x in x_range), color="C1")
             e, = error_axis.plot(range(len(error_development)), error_development, color="black")
 
             pyplot.pause(.001)
@@ -179,13 +195,12 @@ def test_2d():
             l.remove()
             e.remove()
 
-        # r.fit([x], y_t, drag=50)
-        r.fit([x], y_t, drag=iterations)
+        r.fit([x], y_t, drag=100000)
+        # r.fit([x], y_t, drag=iterations)
 
         iterations += 1
 
-    fitted_function = r.fitted_function()
-    l, = plot_axis.plot(x_range, tuple(fitted_function([_x]) for _x in x_range), color="C1")
+    l, = plot_axis.plot(x_range, tuple(r.output([_x]) for _x in x_range), color="C1")
     e, = error_axis.plot(range(len(error_development)), error_development, color="black")
     pyplot.show()
 
